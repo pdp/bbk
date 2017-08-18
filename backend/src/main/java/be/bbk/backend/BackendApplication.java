ffb61808063d873a310abd0b2241b874a239588c
peterdp <peter.de.permentier@gmail.com>
2017-08-18 10:35:21 +0200

    added Dockerfile (don't know why), readme file

--- backend/src/main/java/be/bbk/backend/BackendApplication.java
+++ backend/src/main/java/be/bbk/backend/BackendApplication.java
@@ -1,9 +1,11 @@
 package be.bbk.backend;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class BackendApplication {
 
 	public static void main(String[] args) {
